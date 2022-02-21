e1116faf43ac4a5733a7f07935b466415dfc5172
stanley <stanley.long@fullertonhealth.com>
2022-02-21 15:49:55 +0800

    Adding rotate button.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f5121228a615b6b7ecd07bc928cb220b",
-"index.html": "086dc09908da7cc8ce3c49de8786f145",
-"/": "086dc09908da7cc8ce3c49de8786f145",
-"main.dart.js": "d6db9c2159988fd5d69761d7941b3cc9",
+"index.html": "37155ddf4878096198970fda0d4936de",
+"/": "37155ddf4878096198970fda0d4936de",
+"main.dart.js": "2d1b5bf4cb03158f6f2dc4ca55c225df",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
